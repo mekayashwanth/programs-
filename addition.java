7e4ab3998f7875b712e3faecd60a6bb88f4d3b48
Bhanu Valluri <prakash.1729@gmail.com>
2019-04-16 21:16:35 +0530

    removing files

--- addition.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class addition 
-{
-public static void main(String[] args){
-int a,b,c;
-Scanner add=new Scanner(System.in);
-a=add.nextInt();
-b=add.nextInt();
-c# programs-
